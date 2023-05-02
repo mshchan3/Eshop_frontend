4149e496f04f5026f4154f313c4e66484b1ae428
Marco Chan <mshchan3@gmail.com>
2023-05-02 14:38:40 +0800

    Eshop Done CI/CD

--- src/ui/component/Footer/index.tsx
+++ src/ui/component/Footer/index.tsx
@@ -11,8 +11,7 @@ export default function Footer() {
             <div style={{backgroundColor: "darkgreen", color: "white", display: "flex", alignItems: "center", justifyContent: "center",
             padding: "24px"}}>
                 <h3 style={{marginBottom: 0}}>Join StockX to receive 10% off welcome offer & unlock more rewards!</h3>
-                <br></br>
-                <h3 style={{marginBottom: 0}}>Fuck World</h3>
+                <h3 style={{marginBottom: 0}}><br>Fuck World</br></h3>
             </div>
             <div style={{backgroundColor: "black", color: "white", padding: "24px 0"}}>
                 <Container>
